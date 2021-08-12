cb9f687e61960316ff27b194002e82114d5c0328
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2021-08-12 19:10:47 +0200

    Allow reading template from stdin

--- packages/cli/src/commands/application/create.ts
+++ packages/cli/src/commands/application/create.ts
@@ -4,6 +4,7 @@ import { promises as fs } from 'fs'
 import * as path from 'path'
 
 import AuthenticatedCommand from '../../AuthenticatedCommand'
+import readPipe from '../../util/readPipe'
 
 export default class ApplicationCreate extends AuthenticatedCommand {
   static description = 'Manage Budibase applications.'
@@ -11,6 +12,7 @@ export default class ApplicationCreate extends AuthenticatedCommand {
   static flags = {
     ...AuthenticatedCommand.flags,
     template: flags.string({ char: 't', description: 'use an application template file.', required: false }),
+    'template-stdin': flags.boolean({ char: 's', description: 'read the template string from stdin.', required: false })
   }
 
   static args = [{ name: 'name' }]
@@ -20,12 +22,27 @@ export default class ApplicationCreate extends AuthenticatedCommand {
   async run() {
     const { flags, args } = this.parse(ApplicationCreate)
 
-    try {
-      let templateStr: string | undefined;
-      if (flags.template) {
+    let templateStr: string | undefined;
+
+    if (flags.template) {
+      try {
         const raw = await fs.readFile(path.join(process.cwd(), flags.template))
         templateStr = raw.toString()
+      } catch (e) {
+        console.error('Error reading template file.')
+        throw e
       }
+    } else if (flags['template-stdin']) {
+      const raw = await readPipe()
+      if (raw) {
+        templateStr = raw
+      } else {
+        console.error('No stdin input received.')
+        throw new Error('NO_INPUT')
+      }
+    }
+
+    try {
       const resp = await this.api.application.create(args.name, templateStr)
       console.log(`Created application ${chalk.blueBright(args.name)} (${chalk.blueBright(resp.appId)})`)
     } catch (e) {
